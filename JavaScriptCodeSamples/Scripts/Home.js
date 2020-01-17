ba2431fcab98e619c0ad31f2c9485b64f79a66b2
CHILLI\Mahesh <Mahesh.Moodley@chillisoft.co.za>
2020-01-17 17:01:07 +0200

    Add Javascript code samples

---
+++ JavaScriptCodeSamples/Scripts/Home.js
@@ -0,0 +1,15 @@
+﻿function calculateProfit() {
+    var revenue = document.getElementById("revenue").val();
+    var operatingCost = document.getElementById("operationCost").val();
+    var profit = revenue - operatingCost;
+    document.getElementById("profit").data = profit;
+}
+
+function numberOfDivs() {
+    var numberOfDivs = document.getElementsByTagName("div").length;
+    document.getElementById("output").data = numberOfDivs;
+}
+
+function  numberOfAdditionalCosts(parameters) {
+    var additionalCost = document.getElementsByName("additionalCost").length;
+}
