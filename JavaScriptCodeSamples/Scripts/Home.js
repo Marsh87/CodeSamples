00c2ea0093e9c149477b79d47d78e0b82eab9d3b
CHILLI\Mahesh <Mahesh.Moodley@chillisoft.co.za>
2020-01-24 16:01:35 +0200

    wire javascript to html

--- JavaScriptCodeSamples/Scripts/Home.js
+++ JavaScriptCodeSamples/Scripts/Home.js
@@ -1,15 +1,42 @@
 ﻿function calculateProfit() {
-    var revenue = document.getElementById("revenue").val();
-    var operatingCost = document.getElementById("operationCost").val();
-    var profit = revenue - operatingCost;
-    document.getElementById("profit").data = profit;
+    var revenue = document.getElementById("revenue").value;
+    var operatingCost = document.getElementById("operatingCost").value;
+    var profit = 0;
+    if (revenue && operatingCost) {
+        profit = revenue - operatingCost;
+    }
+    document.getElementById("profit").innerText = "Profit:" + " " + profit;
 }
 
 function numberOfDivs() {
-    var numberOfDivs = document.getElementsByTagName("div").length;
-    document.getElementById("output").data = numberOfDivs;
+    var numOfDivs = document.getElementsByTagName("div").length;
+    alert(numOfDivs);
 }
 
-function  numberOfAdditionalCosts(parameters) {
+function numberOfAdditionalCosts() {
     var additionalCost = document.getElementsByName("additionalCost").length;
+    alert(additionalCost);
+}
+
+function displayFeedbackSection() {
+    var feedbackDiv = document.getElementById("feedbackSection");
+    var feedbackSection =
+        `<div class="container container-padding">
+        <form>
+        <div class="form-group">
+        <label for="revenue">Email</label>
+        <input type="email" class="form-control" id="email" placeholder="Email">
+        </div>
+        <div class="form-group">
+        <label for="operationCost">Comment</label>
+        <textarea class="form-control" id="comment" rows="5" cols="80"></textarea>
+        </div>
+        </form>
+        </div>`;
+    feedbackDiv.innerHTML = feedbackSection;
+}
+
+function clearCommentSection() {
+    var feedbackDiv = document.getElementById("feedbackSection");
+    feedbackDiv.innerHTML = "";
 }
