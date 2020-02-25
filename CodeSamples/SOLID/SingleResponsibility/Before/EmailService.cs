df36f17c4abda7fa157c8904c7b827a43338a986
Mahesh <moodleymahesh@gmail.com>
2020-02-25 10:42:13 +0200

    feat: working on Single Responsibility code samples

---
+++ CodeSamples/SOLID/SingleResponsibility/Before/EmailService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSamples.SOLID.SingleResponsibility.Before
+{
+    public class EmailService:IEmailService
+    {
+        public bool ValidateEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendEmail(MailMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IEmailService
+    {
+        bool ValidateEmail(string email);
+
+        void SendEmail(MailMessage message);
+    }
+}
---
+++ CodeSamples/SOLID/SingleResponsibility/Before/UserService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSamples.SOLID.SingleResponsibility.Before
+{
+    public class UserService
+    {
+        public void Register(string email, string password)
+        {
+            if (!ValidateEmail(email))
+                throw new ValidationException("Email is not an email");
+            var user = new User(email, password);
+
+            SendEmail(new MailMessage("mysite@nowhere.com", email) { Subject = "HEllo foo" });
+        }
+        public virtual bool ValidateEmail(string email)
+        {
+            return email.Contains("@");
+        }
+        public void SendEmail(MailMessage message)
+        {
+            var _smtpClient = new SmtpClient();
+            _smtpClient.Send(message);
+        }
+    }
+}
