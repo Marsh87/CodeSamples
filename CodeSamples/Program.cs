5b16de0019830ae29ef018b1063b99528f52e179
Marsh87 <moodleymahesh@gmail.com>
2020-04-16 18:43:57 +0200

    chore: removed unused usings

--- CodeSamples/Program.cs
+++ CodeSamples/Program.cs
@@ -1,11 +1,5 @@
-﻿// TODO unused usings!
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 using CodeSamples.Helpers;
 using CodeSamples.Models;
 using CodeSamples.Services;
