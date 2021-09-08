33ad4de50865ccc80f74184052cb9d9d3d0091ba
Mahesh Moodley <Mahesh.Moodley@chillisoft.co.za>
2021-09-08 10:50:49 +0200

    feat: add examples on reflection

---
+++ ReflectionCSharpExample/Models/Car.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+
+namespace ReflectionCSharpExample.Models
+{
+    public class Car
+    {
+        public  string Colour { get; set; }
+        public  string Make { get; set; }
+        public  string Model { get; set; }
+        public  double Price { get; set; }
+    }
+}
---
+++ ReflectionCSharpExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using ReflectionCSharpExample.Models;
+
+namespace ReflectionCSharpExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            var type1 = car.GetType();
+            Console.WriteLine(type1.FullName);
+
+            Type type2 = Type.GetType("Reflection.Car", false, true);
+            if (type2 is not null) Console.WriteLine(type2.FullName);
+
+            Type type3 = typeof(Car);
+            Console.WriteLine(type3.FullName);
+            Console.WriteLine(type3.Name);
+            Console.WriteLine(type3.Namespace);
+
+            Type baseType = type3.BaseType;
+            if (baseType is not null) Console.WriteLine(baseType.Name);
+
+            Console.WriteLine(type3.IsAbstract);
+            Console.WriteLine(type3.IsArray);
+            Console.WriteLine(type3.IsClass);
+            Console.WriteLine(type3.IsCOMObject);
+
+            var memberInfo = type3.GetMembers();
+
+            foreach (MemberInfo member in memberInfo)
+            {
+                Console.WriteLine(member.DeclaringType + " " +
+                                  member.MemberType + "  " + member.Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
