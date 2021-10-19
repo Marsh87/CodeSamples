6b1ff7ec0379fe7ad328d306940bcad109fdeee2
Mahesh Moodley <Mahesh.Moodley@chillisoft.co.za>
2021-10-19 19:09:30 +0200

    feat: add code samples for this Global context and Simple function invocation

--- JavaScriptCodeSamples/Scripts/thisKeywordExample.js
+++ JavaScriptCodeSamples/Scripts/thisKeywordExample.js
@@ -14,3 +14,44 @@ const counter = {
 counter.next();
 
 /*The next() is a function that is the property of the counter object. Therefore, inside the next() function, the this references the counter object.*/
+
+
+/*Global Context*/
+/*In the global context, the this references the global object, which is the window object on the web browser or global object on Node.js.*/
+console.log(this === window);
+
+/*If you assign a property to this object in the global context, JavaScript will add the property to the global object as shown in the following example:*/
+this.color= 'Red';
+console.log(window.color); // 'Red'
+
+
+/*Function Context*/
+/*In JavaScript, you can invoke a function in the following ways:
+Function invocation
+Method invocation
+Constructor invocation
+Indirect invocation
+Each function invocation defines its own context, therefore, the this behaves differently than you may expect.
+*/
+
+/*Simple function invocation*/
+/*In the non-strict mode, the this references the global object when the function is called as follows*/
+function show() {
+    console.log(this === window); // true
+}
+
+show();
+/*When you call the show() function, the this references the global object, which is the window on the web browser and global on Node.js.*/
+/*Calling the show() function is the same as:*/
+window.show();
+
+/*In the strict mode, JavaScript sets the this to undefined. Consider the following example:*/
+
+function show() {
+    "use strict";
+    console.log(this === undefined);
+}
+
+show();
+
+
