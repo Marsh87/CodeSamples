0f32cb56b38259360f1c29822020c1689e239e53
Mahesh Moodley <Mahesh.Moodley@chillisoft.co.za>
2022-01-20 18:53:14 +0200

    feat: add MergeSort sorting algorithms

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -2,6 +2,7 @@
 //References
 /*https://www.tutorialspoint.com/selection-sort-program-in-chash#:~:text=Selection%20Sort%20is%20a%20sorting,C%23%20is%20given%20as%20follows.*/
 /*https://www.tutorialspoint.com/chash-program-to-perform-quick-sort-using-recursion#:~:text=Quick%20Sort%20is%20a%20sorting,the%20whole%20array%20is%20sorted.*/
+/*https://dotnettutorials.net/lesson/merge-sort-algorithm-in-csharp/.*/
 
 namespace SortingAlgorithms
 {
@@ -23,6 +24,13 @@ namespace SortingAlgorithms
             int[] arrayToQuickSort = new int[10] { 56, 1, 99, 67, 89, 23, 44, 12, 78, 34 };
             var quickSort = new QuickSort();
             quickSort.Sort(arrayToQuickSort);
+            
+            /*The Merge Sort Algorithm is a comparison-based sorting algorithm that is based on the divide and conquers approach.
+             The divide and conquer approach means it will divide the input array into sub-arrays and then further it divide the sub-arrays into sub-arrays until each sub-array contains a single element.
+              Then each sub-arrays merge together in such a way that it will form a single sorted array. */
+            int[] arrayToMergekSort = new int[10] { 56, 1, 99, 67, 89, 23, 44, 12, 78, 34 };
+            var mergeSort = new MergeSort();
+            mergeSort.Sort(arrayToMergekSort);
         }
     }
 }
