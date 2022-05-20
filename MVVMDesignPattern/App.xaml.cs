1172bd2c094d308646ea5c414a7a02358414fb9c
Mahesh Moodley <Mahesh.Moodley@chillisoft.co.za>
2022-05-20 17:41:35 +0200

    feat: add  MVVM DesignPattern

---
+++ MVVMDesignPattern/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using MVVMDesignPattern.View;
+using MVVMDesignPattern.ViewModel;
+
+
+namespace MVVMDesignPattern
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MVVMDesignPattern.View.MainPage window = new MainPage();
+            UserViewModel VM = new UserViewModel();
+            window.DataContext = VM;
+            window.Show();
+        }
+    }
+}
